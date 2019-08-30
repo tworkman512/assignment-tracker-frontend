1282329042f91c5da0075c2a253a13c4534657b3
Tom Workman <tworkman512@gmail.com>
2019-08-30 15:36:02 -0500

    Add production deployment support

--- src/api/students.js
+++ src/api/students.js
@@ -1,7 +1,7 @@
 const { NODE_ENV } = process.env;
-const BASE_URL = NODE_ENV === "development" ? "http://localhost:5000" : "tbd"; // Once we deploy, we need to change this
-// const { REACT_APP_API_DOMAIN } = process.env;
-// const BASE_URL = REACT_APP_API_DOMAIN;
+// const BASE_URL = NODE_ENV === "development" ? "http://localhost:5000" : "tbd"; // Once we deploy, we need to change this
+const { REACT_APP_API_DOMAIN } = process.env;
+const BASE_URL = REACT_APP_API_DOMAIN;
 
 export const getAllUsers = async () => {
   const token = window.localStorage.getItem("assignment-tracker-app");
